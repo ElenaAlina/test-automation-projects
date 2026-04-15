c9afab30636ca82b75f6358dcf2f1c4b39b5c1cc
Elena Herghelegiu <elenaalina747@gmail.com>
2026-04-15 09:32:32 +0300

    add github actions pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,10 @@
+
 module.exports = {
   allowCypressEnv: false,
 
   e2e: {
+    //to don't executes the tests by its own
+    watchForFileChanges: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
